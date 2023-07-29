6a7ea4635d76655bf901129a377b47474b6f57e2
atique Mujaid Emon <coderc134@gmail.com>
2023-07-29 15:00:44 +0600

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,33 @@ module.exports = {
         '90px' :'90px',
         '100px' :'100px',
         '300px' :'300px',
-  
+        '53px' :'53px',
+        '214px' :'214px',
+        
       },
       opacity: {
         '67': '.67',
-      }
+      },
+      width: {
+        '32': '2rem',
+      },
+      colors: {
+        'bg-first': '#F85559a',
+        'regal-blue': '#243c5a',
+        'regal-blue': '#243c5a',
+        'regal-blue': '#243c5a',
+        'regal-blue': '#243c5a',
+      },
+      screens: {
+        'mobile': '640px',
+        // => @media (min-width: 340px) { ... }
+  
+        'tablet': '1024px',
+        // => @media (min-width: 640px) { ... }
+  
     },
-  },
-  plugins: [],
+  }
+  }
 }
+  plugins: [],
+
